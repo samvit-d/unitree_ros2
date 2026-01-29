5758976f1464c2f58c607f26d272eeb3b90ac19f
samvitd <samvitd@mit.edu>
2026-01-30 04:43:02 +0800

    More environment setup

--- setup_local.sh
+++ setup_local.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Setup unitree ros2 simulation environment"
+source /opt/ros/humble/setup.bash
+source /workspace/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
                             <NetworkInterface name="lo" priority="default" multicast="default" />
